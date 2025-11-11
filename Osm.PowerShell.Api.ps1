c2816a895ed20fae2cfe77d13e76153d63778848
Luke Lloyd <luke.lloyd1985@gmail.com>
2025-11-11 22:52:19 +0000

    printRegisterUrl

--- Osm.PowerShell.Api.ps1
+++ Osm.PowerShell.Api.ps1
@@ -12,6 +12,7 @@ $userRolesUrl = "https://www.onlinescoutmanager.co.uk/api.php?action=getUserRole
 $termsUrl = "https://www.onlinescoutmanager.co.uk/api.php?action=getTerms"
 $membersListUrl = "https://www.onlinescoutmanager.co.uk/ext/members/contact/?action=getListOfMembers"
 $programmeSummaryUrl = "https://www.onlinescoutmanager.co.uk/ext/programme/?action=getProgrammeSummary"
+$printRegisterUrl = "https://www.onlinescoutmanager.co.uk/ext/members/attendance/?action=printRegister"
 
 # Functions
 function Request-Credentials {
