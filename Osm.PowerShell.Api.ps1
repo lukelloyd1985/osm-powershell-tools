e8f11be6007d530b38b14ea3fd50ee315084f9a9
Luke Lloyd <luke.lloyd1985@gmail.com>
2026-01-08 23:24:31 +0000

    programmeAddMeetingUrl

--- Osm.PowerShell.Api.ps1
+++ Osm.PowerShell.Api.ps1
@@ -14,6 +14,7 @@ $membersListUrl = "https://www.onlinescoutmanager.co.uk/ext/members/contact/?act
 $programmeSummaryUrl = "https://www.onlinescoutmanager.co.uk/ext/programme/?action=getProgrammeSummary"
 $accountPreferences = "https://www.onlinescoutmanager.co.uk/v3/settings/account_preferences"
 $printRegisterUrl = "https://www.onlinescoutmanager.co.uk/ext/members/attendance/?action=printRegister&mode=future"
+$programmeAddMeetingUrl = "https://www.onlinescoutmanager.co.uk/ext/programme/?action=addMeeting"
 
 # Functions
 function Request-Credentials {
